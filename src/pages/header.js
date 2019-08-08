a406f94c099e73848838ec70e06b1a3febdbf4f1
aneeshyder <anishcharoliya@gmail.com>
2019-08-08 19:01:45 +0530

    design and menu functionality update

--- src/pages/header.js
+++ src/pages/header.js
@@ -3,6 +3,9 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import '../App.css';
 
 class Header extends Component {
+  openMenu = event =>  document.body.classList.add('nav-active');
+  closeMenu = event =>  document.body.classList.remove('nav-active');
+
   render() {
     return (
       <header id="header" className="py-4">
@@ -10,13 +13,13 @@ class Header extends Component {
           <div className="menu-wrap">
             <Link to="/" className="logo">My App</Link>
               <ul className="nav">
-                <li className="close-button">x</li>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/blog">blog</Link></li>
-                <li><Link to="/contactus">Contact Us</Link></li>
-                <li><Link to="/posts">Posts</Link></li>
+                <li className="close-button" onClick={this.closeMenu}>x</li>
+                <li><Link to="/" onClick={this.closeMenu}>Home</Link></li>
+                <li><Link to="/blog" onClick={this.closeMenu}>blog</Link></li>
+                <li><Link to="/contactus" onClick={this.closeMenu}>Contact Us</Link></li>
+                <li><Link to="/posts" onClick={this.closeMenu}>Posts</Link></li>
               </ul>
-              <span className="menu-box">
+              <span className="menu-box" onClick={this.openMenu}>
                 <span className="mobile-menu"></span>
               </span>
             </div>

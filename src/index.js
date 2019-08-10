a4e61227cf7d1b74dc20594168fe1ca4c6653a6d
anischarolia <anis.charolia@multidots.com>
2019-08-10 10:05:34 +0530

    Modification in src/index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import App from './App'
 import Blog from './pages/blog'
 import Footer from './pages/footer'
@@ -13,13 +14,15 @@ import Topics from './topics'
 
 const routing = (
   <BrowserRouter>
-    <div>     
-      <Route exact path="/" component={App} />
-      <Route path="/blog" component={Blog} />
-      <Route path="/contactus" component={Contactus} />
-      <Route path="/posts" component={Posts} />
-      <Route path='/post/:title' component={Topics} />
-    </div>
+  	<Switch>
+	    <div>     
+	      <Route exact path="/" component={App} />
+	      <Route path="/blog" component={Blog} />
+	      <Route path="/contactus" component={Contactus} />
+	      <Route path="/posts" component={Posts} />
+	      <Route path='/post/:title' component={Topics} />
+	    </div>
+    </Switch>
   </BrowserRouter>
 )
 ReactDOM.render(routing, document.getElementById('root'))
